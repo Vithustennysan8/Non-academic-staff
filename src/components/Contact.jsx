d3cf748f6464127d0a6d0ef0e05e359093a70de5
Vithustennysan <e20416@eng.pdn.ac.lk>
2024-07-10 20:31:22 +0530

    contact page added

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,17 +1,27 @@
-import React from 'react'
-import Header from './Header'
-import Footer from './Footer'
+import Header from "./Header";
+import Footer from "./Footer";
+import "../css/contact.css";
+import ContactCard from "./ContactCard";
 
 const Contact = () => {
   return (
     <>
-      <Header/>
+      <Header />
       <div className="contact_container">
-        
+        <h2>Contact us</h2>
+        <div className="contact_title">
+          <div className="contact-info">
+            <ContactCard position={'Vice Chancellor'} name={'Prof.MD. Lamawansa'} number={'081-2388151'}/>
+            <ContactCard position={'Deputy Vice Chancellor'} name={'Prof.WMT. Madhujith'} number={'081-2389140'}/>
+            <ContactCard position={'Registrar'} name={'Ms.MGJ. Dharmasiri'} number={'081-2387395'}/>
+            <ContactCard position={'Senior Assistant Registrar'} name={'_'} number={'718009811'}/>
+          </div>
+          <div className="contact-info"></div>
+        </div>
       </div>
-      <Footer/>
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
