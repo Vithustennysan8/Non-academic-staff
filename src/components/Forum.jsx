68cb44d5e02f1e8e3e2a6c733c3eed594bfbe05a
Vithustennysan <e20416@eng.pdn.ac.lk>
2024-07-19 09:42:14 +0530

    forum form is handled using useForm hook

--- src/components/Forum.jsx
+++ src/components/Forum.jsx
@@ -6,17 +6,18 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { format } from "date-fns";
+import { useForm } from "react-hook-form";
 
 const Forum = () => {
     const naviagte = useNavigate();
     const [forumPopup, setForumPopup] = useState(false);
     const [forums, setForums] = useState([]);
-    const [addForum, setAddForum] = useState({
-        user:'',
-        subject:'',
-        body:'',
-        createdAt:''
-    });
+    // const [addForum, setAddForum] = useState({
+    //     user:'',
+    //     subject:'',
+    //     body:'',
+    //     createdAt:''
+    // });
 
     const token = localStorage.getItem("token")
     
@@ -43,42 +44,62 @@ const Forum = () => {
         setForumPopup(true);
     }
 
-    const handleChange = (e) =>{
-        const {name,value} = e.target;
-        setAddForum({...addForum, [name]:value}); 
-    }
+    // const handleChange = (e) =>{
+    //     const {name,value} = e.target;
+    //     setAddForum({...addForum, [name]:value}); 
+    // }
     
-    const handleForumSubmit = async (e)=>{
-        e.preventDefault();
+    // const handleForumSubmit = async (e)=>{
+    //     e.preventDefault();
         
-        if(addForum.subject == '' || addForum.body == ''){
-            alert("Please fill all the fields");
-            return;
-        }
-        setForumPopup(false);
+    //     if(addForum.subject == '' || addForum.body == ''){
+    //         alert("Please fill all the fields");
+    //         return;
+    //     }
+    //     setForumPopup(false);
 
-        try {
+    //     try {
 
-            const response = await axios.post("http://localhost:8080/auth/forum/add",addForum,
-                {
-                    headers: {
-                        "Authorization": `Bearer ${token}`
-                        }
-                }
-            )
-            console.log(response.data);
-            setForums([response.data,...forums]);
-            setAddForum({user:'',subject:'',body:'',createdAt:''});
+    //         const response = await axios.post("http://localhost:8080/auth/forum/add",addForum,
+    //             {
+    //                 headers: {
+    //                     "Authorization": `Bearer ${token}`
+    //                     }
+    //             }
+    //         )
+    //         console.log(response.data);
+    //         setForums([response.data,...forums]);
+    //         setAddForum({user:'',subject:'',body:'',createdAt:''});
 
-        } catch (error) {
-            console.log("forumError "+ error);
-        }
-    }
+    //     } catch (error) {
+    //         console.log("forumError "+ error);
+    //     }
+    // }
 
     const handleForumCancel = ()=>{
         setForumPopup(false);
     }
 
+    const {register, handleSubmit, formState : {errors}} = useForm();
+
+    const onSubmit = async(data) => {
+
+        setForumPopup(false);
+        
+        try {
+            const response = await axios.post("http://localhost:8080/auth/forum/add",data,
+                {
+                    headers:{
+                        "Authorization": `Bearer ${token}`
+                    }
+                }
+            )
+            console.log(response.data);
+            setForums([response.data,...forums]);
+        } catch (error) {
+            console.log("useFormError"+error)
+        }
+    }
 
   return (
     <>
@@ -92,20 +113,31 @@ const Forum = () => {
         
         { forumPopup && <div  className='forumInputContainer'>
             
-            <form action="" >
+            <form onSubmit={handleSubmit(onSubmit)} >
                 <div>
                     <label htmlFor="ForumInputSubject">Subject</label>
-                    <input type="text" placeholder="Enter the title" id="forumInputSubject" name="subject" value={addForum.subject} onChange={handleChange}/>
+                    {/* <input type="text" placeholder="Enter the title" id="forumInputSubject" name="subject" value={addForum.subject} onChange={handleChange}/> */}
+                    <input type="text" placeholder="Enter the title" id="forumInputSubject" name="subject" {...register("subject",{required:{
+                        value:true,
+                        message:"Please enter the subject"
+                    }})}/>
+                    {errors.subject && <p className="error">{errors.subject.message}</p>}
                 </div>
                 <div>
                     <label htmlFor="ForumInputContent">content</label>
-                    <textarea name="body" value={addForum.body} onChange={handleChange} id="ForumInputContent" rows={7} placeholder="Enter your thoughts here....."></textarea>
+                    {/* <textarea name="body" value={addForum.body} onChange={handleChange} id="ForumInputContent" rows={7} placeholder="Enter your thoughts here....."></textarea> */}
+                    <textarea name="body" {...register("body",{required:{
+                        value:true,
+                        message:"Please enter your thoughts here"
+                    }})} id="ForumInputContent" rows={7} placeholder="Enter your thoughts here....."></textarea>
+                    {errors.body && <p className="error">{errors.body.message}</p>}
                 </div>
+
                 <div>
                     <label htmlFor="ForumInputFile">Add any files</label>
                     <input type="file" id="ForumInputFile" />
                 </div>
-                <button className="forumFormSubmitbtn" onClick={handleForumSubmit}>Add To Forum</button>
+                <button className="forumFormSubmitbtn" type="submit">Add To Forum</button>
                 <button className="forumFormCancelbtn" onClick={handleForumCancel}>Cancel</button>
             </form>
         </div>
